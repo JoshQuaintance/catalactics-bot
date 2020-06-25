ea393ce251b5ad1aa890ef7a99f4a5eeda128ffa
Joshua Pelealu <45566099+JoshuaPelealu@users.noreply.github.com>
2020-06-25 09:46:32 -0400

    Command Added: about

--- cmd/about.js
+++ cmd/about.js
@@ -3,19 +3,21 @@ const client         = new Discord.Client();
 const newMsg         = require("../functions/newMsg.js");
 const generateFields = require("../functions/generateEmbedFields");
 
+function about(msg) {
+    let moreField = generateFields(
+        ["Who made me", "I was made by someone named Catalactics, He is the owner of this server"],
+        ["Fact about me", "Fun facts! I was made using Discord.js"],
+        ["What can I do", `I can do a number of things, you can get my attention using the \`!\` prefix. To find more about my commands, you can use the \`!commands\` command.`]
+    )
+    let aboutMsg = newMsg("#4364e8", "Let Me Introduce Myself", "Hello there My name is Catalactics Bot.", moreField)
+    .setFooter("That's All, thank you for your attention!")
+
+    msg.channel.send(aboutMsg);
+}
+
 module.exports = {
     prefix         : "about",
     additionalParam: null,
-    description    : "Give the BOT a little description of itself. They deserve some too.",
-    command(msg) {
-        let moreField = generateFields(
-            ["Who made me", "I was made by someone named Catalactics, He is the owner of this server"],
-            ["Fact about me", "Fun facts! I was made using Discord.js"],
-            ["What can I do", `I can do a number of things, you can get my attention using the \`!\` prefix. To find more about my commands, you can use the \`!commands\` command.`]
-        )
-        let aboutMsg = newMsg("#4364e8", "Let Me Introduce Myself", "Hello there My name is Catalactics Bot.", moreField)
-        .setFooter("That's All, thank you for your attention!")
-
-        msg.channel.send(aboutMsg);
-    }
+    desc           : "Give the BOT a little description of itself. They deserve some too.",
+    command        : about
 }

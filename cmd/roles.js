6769f7a390d36b837f3c11f5b568e90c105fc54e
Joshua Pelealu <45566099+JoshuaPelealu@users.noreply.github.com>
2020-06-25 09:46:40 -0400

    Command Added: roles

--- cmd/roles.js
+++ cmd/roles.js
@@ -14,25 +14,30 @@ const main       = require("../index.js");
 const roleSchema = main.roleSchema;
 const memberHasPerm = require("../functions/memberHasPerm.js");    
 
+/**
+ * Function to get all the roles.
+ * @param {Discord.Message} msg Message object
+ */
+async function roles(msg) {
+    let msgText = await newMsg("#2a57eb", "Role List", `Hey there ${msg.author}, here's a list of the roles and what they do.`);
+
+    let Role = mongoose.model("Role", roleSchema, msg.guild);
+    await Role.find({}, (err, data) => {
+        if(err) return console.log(err);
+        
+        data.forEach(role => {
+            msgText.addField(role.roleName, `<@&${role.roleId}> - ${role.desc}`);
+            
+        })
+        // console.log(msgText);
+    });
+    // console.log(msgText);
+    msg.channel.send(msgText);
+}
+
 module.exports = {
     prefix         : "roles",
     additionalParam: null,
     desc           : "Gives a list of the roles and a little description of what they do",
-    async command(msg) {
-        
-        let msgText = await newMsg("#2a57eb", "Role List", `Hey there ${msg.author}, here's a list of the roles and what they do.`);
-
-        let Role = mongoose.model("Role", roleSchema, msg.guild);
-        await Role.find({}, (err, data) => {
-            if(err) return console.log(err);
-            
-            data.forEach(role => {
-                msgText.addField(role.roleName, `<@&${role.roleId}> - ${role.desc}`);
-                
-            })
-            // console.log(msgText);
-        });
-        // console.log(msgText);
-        msg.channel.send(msgText);
-    }
+    command        : roles
 }

dfe7481e2284054b31192277a5e8d3c3e404171b
Joshua Pelealu <45566099+JoshuaPelealu@users.noreply.github.com>
2020-07-02 10:45:07 -0400

    Revert "Whatever I want to go to sleep"
    This reverts commit 4db44dd5f5b78e08370c0970cbf475911dfeb065.

--- functions/getWakeTime.js
+++ functions/getWakeTime.js
@@ -2,8 +2,7 @@ function getWakeTime() {
     const date = new Date();
     const hour = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
     const mins = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
-    const timeZ   = /\((.*)\)/.exec(new Date().toString())[1];
-    const tz = timeZ.match(/[A-Z]+/g).join("");
+    const tz   = /\((.*)\)/.exec(new Date().toString())[1];
     const am_pm = hour >= 12 ? "PM" : "AM";
 
     return `${hour}:${mins} ${am_pm} ${tz}`;

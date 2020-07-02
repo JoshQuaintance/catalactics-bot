4db44dd5f5b78e08370c0970cbf475911dfeb065
Joshua Pelealu <45566099+JoshuaPelealu@users.noreply.github.com>
2020-07-01 22:31:08 -0400

    Whatever I want to go to sleep

--- functions/getWakeTime.js
+++ functions/getWakeTime.js
@@ -2,7 +2,8 @@ function getWakeTime() {
     const date = new Date();
     const hour = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
     const mins = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
-    const tz   = /\((.*)\)/.exec(new Date().toString())[1];
+    const timeZ   = /\((.*)\)/.exec(new Date().toString())[1];
+    const tz = timeZ.match(/[A-Z]+/g).join("");
     const am_pm = hour >= 12 ? "PM" : "AM";
 
     return `${hour}:${mins} ${am_pm} ${tz}`;

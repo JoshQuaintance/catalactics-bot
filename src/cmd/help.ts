6d18d201a94578f94c59762a83be4c95eee8af97
Joshua Pelealu <45566099+JoshuaPelealu@users.noreply.github.com>
2020-07-06 10:52:09 -0400

    Update help command

--- src/cmd/help.ts
+++ src/cmd/help.ts
@@ -1,82 +1,87 @@
 import { Message, MessageEmbed } from 'discord.js';
-import { CMD } from './commands';
-// import { promises } from 'fs';
-import { getSettings } from '../utils/get-settings';
 import mongoose from 'mongoose';
 import { commandUsageSchema } from '../index';
 import { CommandDbType } from '../utils/db-defs/commandDB.int';
 import { CommandsType } from '../utils/cmd-def.int';
+import { config } from 'dotenv/types';
 
-// const { readdir } = promises;
-// Settings
-const prefix = getSettings().PREFIX;
-const URI = getSettings().MONGO_URI;
 
-// Connect to the database using the URI from the variable file.
-mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .catch(err => console.log(err));
-
-/**
- * Get the top 4 commands with the most called times.
- */
-async function getPopularCommands(msg: Message) {
-	let guild = msg.guild?.toString();
-
-	const Data = mongoose.model('Commands', commandUsageSchema, guild);
-    let sorted: any;
-
-	await Data.find({}, (err: any, data: CommandDbType[]) => {
-        if (err) throw err;
-
-        let filtered = data.filter(doc => typeof doc.prefix == 'string');
-        sorted = filtered.sort((a, b) => b.amountCalled - a.amountCalled).slice(0, 4);
-    });
-    console.log(sorted + "Sorted")
-    return sorted;
-}
-
-/**
- * Get all the commands and display it.
- * @param {Discord.Message} msg Message Object
- */
 export const help: CommandsType = {
-	prefix          : 'help',
-	additionalParam : '[Command]',
-	desc            :
+	prefix: 'help',
+	additionalParam: '[Command]',
+	desc:
 		"Displays all of the commands available. If a command is specified, it will display the description of the command. That's this command",
-	command         :  async function getAllCommands(msg: Message) {
-        try {
-            let embedMsg = new MessageEmbed()
-			.setColor('#23f0c7')
-			.setTitle('List of Commands')
-			.setDescription(`Hey there ${msg.author}, I have sent you the full list of all the commands available. Here's a short list of the common commands.`);
+	/**
+     * Get all the commands and display it.
+     * @param {Discord.Message} msg Message Object
+     */
+	command: async function getAllCommands(msg, { settings, CMD }) {
+		try {
+            // Connect to the database using the URI from the variable file.
+            mongoose.connect(settings.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+                .catch(err => {throw err});
+			const Data = mongoose.model('Commands', commandUsageSchema, msg.guild!.toString());
 
-            let cmdMsg = new MessageEmbed()
-                .setColor('#23f0c7')
-                .setTitle('Full List of All The Commands Available')
-                .setDescription(`Hello there, here is the full list of all the commands available. If you want to call me, you can do so using the prefix \`${prefix}\``)
+			// Gets the argument
+			let splitMsg = msg.content.split(' ')[1];
 
-            await getPopularCommands(msg).then(fulfilled => {
-                fulfilled.forEach((cmd: any) => {
-                    let lookup = CMD.find(look => cmd.prefix == look.prefix);
-                    embedMsg.addField(lookup!.prefix, lookup!.desc);
+			// Finds the command using the prefix and return the object.
+			const commandFound = CMD.find((command: any) => command.prefix == splitMsg);
+
+			// If there is no argument, it's going to send the full list
+			if (splitMsg == undefined || commandFound == undefined) {
+				let embedMsg = new MessageEmbed()
+					.setColor('#23f0c7')
+					.setTitle('List of Commands')
+					.setDescription(
+						`Hey there ${msg.author}, Here's a full list of all the commands. \n For more information about each commands, type \`${settings.PREFIX}help <command>\``
+					);
+
+                await Data.find({}, (err: any, data: CommandsType[]) => {
+                    if(err) throw err;
+
+                    let x: any[] = []
+                    data.forEach(cmd => {
+                        if(!cmd.prefix) return;
+                        x.push(`\`${cmd.prefix}\``)
+                    })
+                    let xx = x.join(' ');
+
+                    embedMsg.addField(`All Commands`, xx)
                 });
-            });
 
+				msg.channel.send(embedMsg);
+			} else {
+				// Else (if there is an argument)
+				// Determine what the content of the title should be according if the
+				// Command found has an additional Param or not
+				let title = commandFound.additionalParam
+					? `\`${commandFound.prefix} ${commandFound.additionalParam}\``
+					: `\`${commandFound.prefix}\``;
 
-            CMD.forEach(file => {
+				const embed = new MessageEmbed()
+					.setColor('#42f5dd')
+					.setTitle(title)
+					.addField('Description', commandFound.desc);
 
-                if (file.prefix && file.desc)
-                    cmdMsg.addField(file.prefix, file.desc);
-            });
+				/**
+                 * If the command have additionalParam, it will add new
+                 * For the options
+                 */
+				if (commandFound.additionalParam)
+					embed.addField('Additional Parameter(s)', commandFound.additionalParam);
 
+				await Data.findOne({ prefix: commandFound.prefix }, (err: any, data: CommandDbType) => {
+					if (err) throw err;
+					embed.addField('Amount Called in Server', `${data.amountCalled} times`);
+				});
 
-            msg.author.send(cmdMsg);
-            msg.channel.send(embedMsg);
-        } catch (err) {
-            console.error(err);
-        }
-     }
-
+				msg.channel.send(embed);
+			}
+		} catch (err) {
+			console.error(err);
+		}
+	}
 };
 
+export default help;

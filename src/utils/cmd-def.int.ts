2fdae1d5e1e22de1051666a822be8d28fca0fbac
Joshua Pelealu <45566099+JoshuaPelealu@users.noreply.github.com>
2020-07-06 10:56:18 -0400

    Added Client, Settings, and CMD into the parameter

--- src/utils/cmd-def.int.ts
+++ src/utils/cmd-def.int.ts
@@ -1,9 +1,16 @@
 import { Message, Client } from 'discord.js';
+import { Settings } from './get-settings';
+
+export interface CommandsArgs {
+    client: Client;
+    settings: Settings;
+    CMD: CommandsType[];
+}
 
 export interface CommandsType {
     prefix: string;
     additionalParam?: string;
     args?: string | {}[];
     desc: string;
-    command: (msg: Message, client?: Client) => void;
+    command: (msg: Message, args: CommandsArgs) => void;
 }

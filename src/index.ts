e2e197b19fe2dec747c7ecc488f3b5f97e17ef3d
Joshua Pelealu <45566099+JoshuaPelealu@users.noreply.github.com>
2020-07-19 11:41:49 -0400

    Testing

--- src/index.ts
+++ src/index.ts
@@ -36,8 +36,7 @@ client.once('ready', async () => {
     setAllCommands();
     const startTime: string = wakeUpTime();
     console.log(color.red(`Logged in as ${client.user!.tag}`));
-    cli.log(color.red('Testing'))
-    process.stdout.write(color.red('Text'), (err) => {if (err) console.error(err)});
+    cli.log('string', color.red('Testing'))
 
 	if (settings.LOG_WHEN_ONLINE) {
 		let msg: Discord.MessageEmbed = new MessageEmbed()

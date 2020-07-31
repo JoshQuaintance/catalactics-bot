53beca65b4b801a5a27eaf10769235937b790813
Joshua Pelealu <45566099+JoshuaPelealu@users.noreply.github.com>
2020-07-30 20:50:10 -0400

    Lint

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-	preset            : 'ts-jest',
-	testEnvironment   : 'node',
-	verbose           : true,
-	errorOnDeprecated : true,
-	notify            : true
+  preset            : 'ts-jest',
+  testEnvironment   : 'node',
+  verbose           : true,
+  errorOnDeprecated : true,
+  notify            : true
 };
